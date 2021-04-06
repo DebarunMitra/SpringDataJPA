c915401273563c5fb967a27eb6750c67e306fab5
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2021-04-06 17:56:51 +0530

    basic setup added

---
+++ src/main/java/SpringDataJPAapplication/springDataJPAapplication/SpringDataJpAapplicationApplication.java
@@ -0,0 +1,13 @@
+package SpringDataJPAapplication.springDataJPAapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpAapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpAapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringDataJPAapplication/springDataJPAapplication/SpringDataJpAapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringDataJPAapplication.springDataJPAapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpAapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
